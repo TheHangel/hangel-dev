c4ae3a722159c64061177836ff1749c3f3cb6e99
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2023-08-26 22:19:39 +0200

    overlay-item component

--- src/components/minecraft/VItem.vue
+++ src/components/minecraft/VItem.vue
@@ -5,19 +5,10 @@
         :src="item.texture"
         :alt="item.name"
     >
-    <div v-else-if="item.name && (item.texture instanceof Object)" >
-        <img
-            class="aspect-square float-left z-10"
-            :src="item.texture.base"
-            :alt="item.name"
-        >
-        <img
-            class="aspect-square float-left z-20 -ml-40"
-            :src="item.texture.overlay"
-            :alt="item.name"
-            style="filter: invert(20%) sepia(53%) saturate(6725%) hue-rotate(358deg) brightness(97%) contrast(135%);"
-        >
-    </div>
+    <VOverlayItem
+        v-else-if="item.name && (item.texture instanceof Object)"
+        :item="item"
+    />
     <div
         v-else
         class="w-32 h-32 float-left aspect-square border border-gray-400 bg-gray-200"
@@ -26,13 +17,18 @@
 </template>
 
 <script>
+import VOverlayItem from './VOverlayItem.vue';
 
 export default {
+    components: {
+        VOverlayItem
+    },
     props: {
         item: {
             type: Object,
-            default: () => {}
+            default: () => { }
         }
-    }
+    },
+    components: { VOverlayItem }
 }
 </script>
---
+++ src/components/minecraft/VOverlayItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img
+            class="aspect-square float-left z-10"
+            :src="item.texture.base"
+            :alt="item.name"
+        >
+        <img
+            class="aspect-square float-left z-20 -ml-40"
+            :src="item.texture.overlay"
+            :alt="item.name"
+            style="filter: invert(20%) sepia(53%) saturate(6725%) hue-rotate(358deg) brightness(97%) contrast(135%);"
+        >
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        item: {
+            type: Object,
+            default: () => {}
+        }
+    }
+}
+</script>
