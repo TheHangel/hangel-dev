d1b4b1baad2b698e72351c95688096eb53932b1c
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2023-08-26 18:57:03 +0200

    tooltip

---
+++ src/components/minecraft/VTooltip.vue
@@ -0,0 +1,46 @@
+<template>
+    <div
+        class="mouse-follow-text relative overflow-hidden h-full w-full"
+        @mousemove="updatePosition"
+        @mouseenter="isVisible = true"
+        @mouseleave="isVisible = false"
+    >
+        <div
+            v-if="isVisible"
+            :style="{ top: mouseY + 'px', left: mouseX + 'px' }"
+            class="text-container absolute whitespace-nowrap bg-black text-white p-2 rounded pointer-events-none"
+        >
+            {{ $t(text) }}
+        </div>
+    </div>
+</template>
+
+<script>
+import VItem from './VItem.vue';
+
+export default {
+    components: {
+        VItem
+    },
+    data() {
+        return {
+            mouseX: 0,
+            mouseY: 0,
+            isVisible: false,
+            text: "glass_bottle"
+        };
+    },
+    methods: {
+        updatePosition(event) {
+            this.mouseX = event.clientX;
+            this.mouseY = event.clientY;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.mouse-follow-text {
+    height: 100vh;
+}
+</style>
