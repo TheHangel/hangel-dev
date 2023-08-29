1748c9956b2f1263bd49c39e3fe8b3a0ad79ef7c
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2023-08-30 00:47:16 +0200

    refractor tooltip / css

--- src/components/minecraft/VTooltip.vue
+++ src/components/minecraft/VTooltip.vue
@@ -1,10 +1,23 @@
 <template>
     <div
-        ref="divSouris"
+        ref="divTooltip"
         class="absolute bg-black opacity-80 select-none text-center align-items-center pt-3 pl-2 pr-2 -mt-3"
     >
-        <p v-if="effectName" class="text-white text-lg !opacity-100" style="text-shadow: 2px 2px rgb(93, 93, 93); font-family: 'minecraftia';">{{ $t(itemName) }} {{ $t('of') }} {{ $t(effectName) }}</p>
-        <p v-else class="text-white text-lg !opacity-100" style="text-shadow: 2px 2px rgb(93, 93, 93); font-family: 'minecraftia';">{{ $t(itemName) }}</p>
+        <p
+            v-if="effectName"
+            class="text-white text-lg !opacity-100 font-minecraftia"
+            style="text-shadow: 2px 2px rgb(93, 93, 93);"
+        >
+            {{ $t(itemName) }} {{ $t('of') }} {{ $t(effectName) }}
+        </p>
+        <p
+            v-else
+            class="text-white text-lg !opacity-100
+            font-minecraftia"
+            style="text-shadow: 2px 2px rgb(93, 93, 93);"
+        >
+            {{ $t(itemName) }}
+        </p>
     </div>
 </template>
 
@@ -23,17 +36,21 @@ export default {
         }
     },
     setup() {
-        const divSouris = ref(null);
 
-        watch(divSouris, () => {
-            window.addEventListener("mousemove", function(event) {
-                if(divSouris.value !== null) {
-                    divSouris.value.style.left = (event.clientX+10) + "px";
-                    divSouris.value.style.top = (event.clientY-25) + "px";
+        // data
+        const divTooltip = ref(null);
+
+        // watchers
+        watch(divTooltip, () => {
+            window.addEventListener("mousemove", (event) => {
+                if(divTooltip.value !== null) {
+                    divTooltip.value.style.left = (event.clientX+10) + "px";
+                    divTooltip.value.style.top = (event.clientY-25) + "px";
                 }
             });
         });
-        return { divSouris };
+
+        return { divTooltip };
     }
 }
 </script>
