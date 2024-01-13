6ef923a0cbdecab22cc2f8ed1f9e5de18cb560a8
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2024-01-14 00:46:56 +0100

    miniatures, section centrée

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -2,37 +2,43 @@ export default [
     {
         name: "Quarto",
         categorie: "Jeu",
-        description: "Petit jeu développé en C se jouant dans le terminal.",
-        thumbnail: "/src/assets/quarto_thumbnail.png"
+        description: "Petit jeu développé en C permettant de joueur au Quarto dans le terminal.",
+        thumbnail: "/src/assets/projects/quarto_thumbnail.png",
+        link: ""
     },
     {
         name: "Fallout Hacking",
         categorie: "Jeu",
-        description: "",
-        thumbnail: ""
+        description: "Petit jeu développé en C reproduisant un mini-jeu de Fallout dans le terminal.",
+        thumbnail: "/src/assets/projects/fallout_thumbnail.png",
+        link: ""
     },
     {
         name: "The Fletching Table Mod",
         categorie: "Mod",
         description: "",
-        thumbnail: ""
+        thumbnail: "/src/assets/projects/the_fletching_table_mod_thumbnail.png",
+        link: ""
     },
     {
         name: "Old Combat Mod",
         categorie: "Mod",
         description: "",
-        thumbnail: ""
+        thumbnail: "/src/assets/projects/old_combat_mod_thumbnail.png",
+        link: ""
     },
     {
         name: "TP Librairie",
         categorie: "Logiciel",
         description: "",
-        thumbnail: ""
+        thumbnail: "/src/assets/projects/tp_editeur_thumbnail.png",
+        link: ""
     },
-    {
+    /*{
         name: "Hangel Music App",
         categorie: "Logiciel",
         description: "",
-        thumbnail: ""
-    }
+        thumbnail: "",
+        link: ""
+    }*/
 ];
--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -1,19 +1,27 @@
 <template>
-<h1 class="text-center">Projets</h1>
-<section class="flex flex-wrap m-24">
-    <article
-        v-for="project in projects"
-        class="bg-white w-64 h-64 shadow-lg rounded-lg m-8"
-    >
-        <img :src="project.thumbnail" alt="Description" class="w-full">
-        <div class="text-center p-4">
-            <p class="text-black">{{ project.description }}</p>
-        </div>
-        <div class="text-center">
-            <a href="#" class="inline-block w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">En savoir plus</a>
-        </div>
-    </article>
-</section>
+    <h1 class="text-center">Projets</h1>
+    <div class="flex justify-center items-center">
+        <section class="flex flex-wrap justify-center mt-0 mb-4">
+            <article
+                v-for="project in projects"
+                class="bg-white w-64 h-72 shadow-lg rounded-lg m-8"
+            >
+                <img
+                    :src="project.thumbnail"
+                    alt="Description"
+                    class="w-full"
+                >
+
+                <p class="text-center font-bold text-lg pt-4 text-black">{{ project.name }}</p>
+                <p class="text-center p-4 text-black text-justify">{{ project.description }}</p>
+
+                <div class="text-center">
+                    <a href="#" class="inline-block w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">En savoir plus</a>
+                </div>
+                
+            </article>
+        </section>
+    </div>
 </template>
 
 <script>
