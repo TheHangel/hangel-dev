c1af74ed8d19d8d2668f2df302d850bb1ce4448b
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2024-01-13 23:44:43 +0100

    projects from list

---
+++ src/constants/projects.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "Quarto",
+        categorie: "Jeu",
+        description: "Projet de jeu développé en C se jouant dans le terminal.",
+        thumbnail: "/src/assets/quarto_thumbnail.png"
+    },
+    {
+        name: "Fallout Hacking",
+        categorie: "Jeu",
+        description: "",
+        thumbnail: ""
+    },
+    {
+        name: "The Fletching Table Mod",
+        categorie: "Mod",
+        description: "",
+        thumbnail: ""
+    },
+    {
+        name: "Old Combat Mod",
+        categorie: "Mod",
+        description: "",
+        thumbnail: ""
+    },
+    {
+        name: "TP Librairie",
+        categorie: "Logiciel",
+        description: "",
+        thumbnail: ""
+    },
+    {
+        name: "Hangel Music App",
+        categorie: "Logiciel",
+        description: "",
+        thumbnail: ""
+    }
+];
--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -5,26 +5,25 @@
         <h2>Jeux</h2>
         <hr size="6" width="80%" color="#2f2f30">
     </div>
-    <section class="jeux">
-        <article class="quarto">
-            <img src="images/quarto/quarto_thumbnail.png" alt="quarto_thumbnail">
-            <p class="normal">Projet de jeu développé en C, se jouant dans le terminal UNIX.</p>
-            <button onclick="window.location.href='projets/quarto.html'">En savoir plus</button>
-        </article>
-    </section>
-    <div class="mods-text">
-        <h2>Mods</h2>
-        <hr size="6" width="80%" color="#2f2f30">
-    </div>
-    <section class="mods">
-        <article class="oldCombat">
-            <p class="normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sunt?</p>
-            <button>En savoir plus</button>
-        </article>
-        <article class="fletching">
-            <p class="normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sunt?</p>
+    <section class="projects">
+        <article v-for="project in projects">
+            <img :src="project.thumbnail" alt="project_thumbnail">
+            <p class="normal">{{ project.description }}</p>
             <button>En savoir plus</button>
         </article>
     </section>
 </div>
 </template>
+
+<script>
+import projects from '@/constants/projects';
+
+export default {
+    setup() {
+        return {
+            projects
+        }
+    }
+}
+
+</script>
