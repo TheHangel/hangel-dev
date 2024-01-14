f5a9e000b7a70d9aaf7190f0b8ff3441c67c6694
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2024-01-14 01:13:36 +0100

    alignement boutons

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -2,43 +2,48 @@ export default [
     {
         name: "Quarto",
         categorie: "Jeu",
+        date: "2021",
         description: "Petit jeu développé en C permettant de joueur au Quarto dans le terminal.",
         thumbnail: "/src/assets/projects/quarto_thumbnail.png",
-        link: ""
+        link: "https://github.com/TheHangel/quarto-game"
     },
     {
         name: "Fallout Hacking",
         categorie: "Jeu",
+        date: "2022",
         description: "Petit jeu développé en C reproduisant un mini-jeu de Fallout dans le terminal.",
         thumbnail: "/src/assets/projects/fallout_thumbnail.png",
-        link: ""
+        link: "https://github.com/TheHangel/fallout4-hacking-minigame"
     },
     {
         name: "The Fletching Table Mod",
         categorie: "Mod",
-        description: "",
+        date: "2021",
+        description: "Mod Minecraft ajoutant une fonctionnalité à la table d'archerie.",
         thumbnail: "/src/assets/projects/the_fletching_table_mod_thumbnail.png",
-        link: ""
+        link: "https://modrinth.com/mod/the-fletching-table-mod"
     },
     {
         name: "Old Combat Mod",
         categorie: "Mod",
-        description: "",
+        date: "2020",
+        description: "Mod Minecraft permettant d'utiliser l'ancien système de combat du jeu.",
         thumbnail: "/src/assets/projects/old_combat_mod_thumbnail.png",
-        link: ""
+        link: "https://modrinth.com/mod/old-combat-mod"
     },
     {
         name: "TP Librairie",
         categorie: "Logiciel",
-        description: "",
+        date: "2022",
+        description: "Projet de TP réalisé en C# permettant d'intéragir avec une base de données d'une librairie.",
         thumbnail: "/src/assets/projects/tp_editeur_thumbnail.png",
-        link: ""
+        link: "https://github.com/TheHangel/tp-bdd-editeur"
     },
     /*{
         name: "Hangel Music App",
         categorie: "Logiciel",
         description: "",
         thumbnail: "",
-        link: ""
+        link: "https://github.com/TheHangel/hangel-music-app"
     }*/
 ];
--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -2,24 +2,27 @@
     <h1 class="text-center">Projets</h1>
     <div class="flex justify-center items-center">
         <section class="flex flex-wrap justify-center mt-0 mb-4">
-            <article
-                v-for="project in projects"
-                class="bg-white w-64 h-72 shadow-lg rounded-lg m-8"
-            >
-                <img
-                    :src="project.thumbnail"
-                    alt="Description"
-                    class="w-full"
+            <div v-for="project in projects" class="m-8">
+                <article class="bg-white w-72 h-80 shadow-lg rounded-t-lg">
+                    <img
+                        :src="project.thumbnail"
+                        alt="Description"
+                        class="w-full"
+                    >
+
+                    <p class="text-center font-bold text-lg pt-4 text-black">{{ project.name }}</p>
+                    <p class="text-center text-slate-500 pt-2">{{ project.categorie }} - {{ project.date }}</p>
+                    <p class="text-center pt-2 px-6 text-black text-justify">{{ project.description }}</p>
+                    
+                </article>
+                <a
+                    :href="project.link"
+                    class="inline-block text-center w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
                 >
+                    En savoir plus
+                </a>
 
-                <p class="text-center font-bold text-lg pt-4 text-black">{{ project.name }}</p>
-                <p class="text-center p-4 text-black text-justify">{{ project.description }}</p>
-
-                <div class="text-center">
-                    <a href="#" class="inline-block w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">En savoir plus</a>
-                </div>
-                
-            </article>
+            </div>
         </section>
     </div>
 </template>
