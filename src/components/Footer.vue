0c83573377e20e9c3e4a5b7446018221e5d65c1b
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2024-01-14 01:26:51 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,9 @@
 <template>
-    <footer>
-        <div class="content-footer">
-            <p class="copyright">© 2024 Lebrasseur Thibaud</p>
-            <div class="container-links">
-                <span><a href="https://www.twitter.com/thibaud_leb"><i class="fab fa-twitter"></i></a></span>
-                <span><a href="https://www.instagram.com/thibaud_leb"><i class="fab fa-instagram"></i></a></span>
-            </div>
+    <div class="bg-gray-800 text-white text-center p-4 absolute bottom-0 w-full">
+        <p class="copyright">© 2021 Lebrasseur Thibaud</p>
+        <div class="container-links">
+            <span><a href="https://www.twitter.com/thibaud_leb"><i class="fab fa-twitter"></i></a></span>
+            <span><a href="https://www.instagram.com/thibaud_leb"><i class="fab fa-instagram"></i></a></span>
         </div>
-        </footer>
+    </div>
 </template>
