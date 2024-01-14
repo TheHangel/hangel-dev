22a3b2b17cd7f8e7ed3c7e25b80225819a7330fc
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2024-01-14 01:30:48 +0100

    réseaux sociaux boutons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="bg-gray-800 text-white text-center p-4 absolute bottom-0 w-full">
-        <p class="copyright">© 2021 Lebrasseur Thibaud</p>
-        <div class="container-links">
-            <span><a href="https://www.twitter.com/thibaud_leb"><i class="fab fa-twitter"></i></a></span>
-            <span><a href="https://www.instagram.com/thibaud_leb"><i class="fab fa-instagram"></i></a></span>
+        <p>© 2021 Lebrasseur Thibaud</p>
+        <div>
+            <span class="mx-2 text-button"><a href="https://www.twitter.com/thibaud_leb"><i class="fab fa-twitter"></i></a></span>
+            <span class="mx-2 text-button"><a href="https://www.instagram.com/thibaud_leb"><i class="fab fa-instagram"></i></a></span>
+            <span class="mx-2 text-button"><a href="https://www.linkedin.com/in/thibaud-lebrasseur/"><i class="fab fa-linkedin"></i></a></span>
+            <span class="mx-2 text-button"><a href="https://github.com/TheHangel/"><i class="fab fa-github"></i></a></span>
         </div>
     </div>
 </template>
